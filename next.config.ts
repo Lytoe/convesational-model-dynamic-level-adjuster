276b1129d2ef8938c83b1f8019f1ff78b8883625
Nima Taherkhani <n.taherkhani1378@gmail.com>
2025-11-23 18:13:06 +0100

    fixed next configs

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // If you ever want to deploy as a Docker container or use Vercel/standalone
-  // output: 'standalone',  
-  // You can add future options here as needed, such as images or experimental flags.
+  reactStrictMode: true
+  // Future options here (output: 'standalone', images, experimental, etc)
 };
 
 export default nextConfig;
